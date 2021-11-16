65bf6ce4cc544ac91c03a4c0196fc02ec752f7e7
ericfp87 <52512077+ericfp87@users.noreply.github.com>
2021-11-16 15:53:59 -0300

    Secrets

--- app.js
+++ app.js
@@ -41,7 +41,8 @@ const userSchema = new mongoose.Schema ({
     email: String,
     password: String,
     //LEVEL 6
-    googleId: String
+    googleId: String,
+    secret: String
 });
 
 //LEVEL 5
@@ -106,11 +107,39 @@ app.get("/register", function(req, res){
 
 //LEVEL 5
 app.get("/secrets", function(req, res){
+    User.find({"secret": {$ne: null}}, function(err, foundUsers){
+        if (err) {
+            console.log(err);
+        } else {
+            if (foundUsers){
+                res.render("secrets", {usersWithSecrets: foundUsers})
+            }
+        }
+    });
+});
+
+app.get("/submit", function(req, res){
     if (req.isAuthenticated()){
-        res.render("secrets");
+        res.render("submit");
     } else {
         res.redirect("/login");
     }
+});
+
+app.post("/submit", function(req,res){
+    const submittedSecret = req.body.secret;
+    User.findById(req.user.id, function(err, foundUser){
+        if (err) {
+            console.log(err);
+        } else {
+            if (foundUser) {
+                foundUser.secret = submittedSecret;
+                foundUser.save(function(){
+                    res.redirect("/secrets");
+                })
+            }
+        }
+    })
 });
 
 //LEVEL 5
